0c9a22ec25568cc3625e0b0ceddd7435a0969a67
marklabenski <mark.labenski@gmail.com>
2015-10-31 19:25:23 +0100

    refactor: shrink file to own require implementation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,123 +2,25 @@
 /**
  * Created by marklabenski on 31.10.15.
  */
-var renderer = PIXI.autoDetectRenderer(800, 600, {backgroundColor : 0x1099bb});
-document.body.appendChild(renderer.view);
+ // pixi exposes a premade instance for you to use.
 
-var loader = PIXI.loader; // pixi exposes a premade instance for you to use.
-
-
-loader.add('player','img/player.png');
-
-loader.once('complete',setup);
-
-loader.load();
-
-window.game = {};
-window.game.getBounds = function getBounds() {
-    return {x: 800, y:600};
-}
-
-var createPlayer = function createPlayer() {
-    var playerInstance;
-    var sprite;
-
-    //init with texture
-    if(arguments[0]) {
-        sprite = new PIXI.Sprite(arguments[0]);
-        sprite.scale.x = 0.5;
-        sprite.scale.y = 0.5;
-
-        sprite.anchor.x = 0.5;
-        sprite.anchor.y = 0.5;
-
-        sprite.position.x = 0+ sprite.width/2;
-        sprite.position.y = 0+ sprite.width/2;
-    }
-    var DIRECTION =  {DOWN:0, LEFT:1, TOP:2, RIGHT:3};
-    var direction = 0;
-    var changeDirection = function changeDirection(change) {
-        var newDirection = direction + change;
-        if (direction === DIRECTION.DOWN && change === -1) {
-            newDirection = DIRECTION.RIGHT;
-        } else if (direction == DIRECTION.RIGHT && change === 1){
-            newDirection = DIRECTION.DOWN;
+window.require = function require(scripts, done) {
+    var dependencies = [];
+    var dfd = $.Deferred();
+    if(!Array.isArray(scripts))
+        scripts = [scripts];
+    scripts.map(function (script) {
+        if(dependencies.indexOf(script) === -1) {
+            dfd.done($.getScript("scripts/" + script));
+            dependencies.push(script);
         }
-        direction = newDirection;
-        return direction;
-    };
-    var movementIsValid = function movementIsValid(movement) {
-        return sprite.position.x + movement.x > 0
-            && sprite.position.y + movement.y > 0
-            && sprite.position.x + movement.x < window.game.getBounds().x
-            && sprite.position.y + movement.y < window.game.getBounds().y
-    };
-    var Player = {
-        getSprite: function getSprite() {
-            return sprite;
-        },
-        move: function move() {
-            var movement = {x:0, y:0};
-            if(direction === DIRECTION.DOWN) {
-                movement.y += sprite.width;
-            } else if (direction === DIRECTION.LEFT) {
-                movement.x -= sprite.width;
-            } else if (direction === DIRECTION.TOP) {
-                movement.y -= sprite.width;
-            } else {
-                movement.x += sprite.width;
-            }
-            if(movementIsValid(movement)) {
-                sprite.position.x += movement.x;
-                sprite.position.y += movement.y;
-            }
-        },
-        changeDirectionByKeyCode: function changeDirectionByKeyCode(keyCode) {
-            switch (keyCode) {
-                case 37: // arrow left
-                    sprite.rotation = changeDirection(-1) * (Math.PI * 0.5);
-                    break;
-                case 39: // arrow right
-                    // Do something for "right arrow" key press.
-                    sprite.rotation = changeDirection(1) * (Math.PI * 0.5);
-                    break;
-                default:
-                    return; // Quit when this doesn't handle the key event.
-            }
-        },
-    };
-
-    return playerInstance || (playerInstance = Object.create(Player));
-};
-
-function setup() {
-    // create the root of the scene graph
-    var stage = new PIXI.Container();
-
-// create some textures from an image path
-    var playerTexture = PIXI.Texture.fromImage('img/player.png');
-
-    var player = createPlayer(playerTexture);
-
-    stage.addChild(player.getSprite());
-
-    renderer.render(stage);
-    animate();
-    function animate() {
-        requestAnimationFrame(animate);
-
-        // render the container
-        renderer.render(stage);
-    }
-
-    window.setInterval(function() {
-        player.move();
-
-    },1000);
-    window.addEventListener('keydown', function(event) {
-
-        player.changeDirectionByKeyCode(event.keyCode);
     });
 
+    dfd.done(function () {done()});
+    dfd.resolve();
+};
 
-}
+require('feierabend/game.js', function(){ debugger;});
+
+
+
