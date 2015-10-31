6c0f06cbfc0ef72d4c7ecd031071bc20a835f02c
marklabenski <mark.labenski@gmail.com>
2015-10-31 20:53:46 +0100

    refactor(Game): wrap up all the stuff in the game object

--- app/scripts/feierabend/game.js
+++ app/scripts/feierabend/game.js
@@ -2,55 +2,81 @@
  * Created by marklabenski on 31.10.15.
  */
 
-require('feierabend/player.js', function () {
-    window.game = {};
+require('feierabend/player.js', function (player) {
+    var gameWidth = 800;
+    var gameHeight = 600;
+    var gridSize = 50;
 
-    var renderer = PIXI.autoDetectRenderer(800, 600, {backgroundColor: 0x1099bb});
+    var assets = [
+        { name: 'player', file: 'img/player.png'}
+    ];
+
+    var renderer = PIXI.autoDetectRenderer(gameWidth, gameHeight, {backgroundColor: 0x1099bb});
     document.body.appendChild(renderer.view);
 
     var loader = PIXI.loader;
 
+    assets.map(function(asset) {
+        loader.add(asset.name, asset.file);
+    });
 
-    window.game.getBounds = function getBounds() {
-        return {x: 800, y: 600};
-    };
-
-    loader.add('player', 'img/player.png');
-
-    loader.once('complete', setup);
-
-    loader.load();
-
-
-    function setup() {
-        // create the root of the scene graph
+    var createGame = function createGame() {
         var stage = new PIXI.Container();
+        var player;
+        var counters = {};
 
-        // create some textures from an image path
-        var playerTexture = PIXI.Texture.fromImage('img/player.png');
+        var onElapsed = function (delta, currentTimestamp) {
+            currentTimestamp = currentTimestamp || 0;
+            if(!counters[delta])
+                counters[delta] = {elapsed: 0, lastTimestamp: currentTimestamp};
 
-        var player = createPlayer(playerTexture);
+            var counter = counters[delta];
 
-        stage.addChild(player.getSprite());
+            counter.elapsed = currentTimestamp - counter.lastTimestamp;
 
-        renderer.render(stage);
-        animate();
-        function animate() {
-            requestAnimationFrame(animate);
+            return function (isElapsed) {
+                if(counter.elapsed > delta) {
+                    counter.lastTimestamp = currentTimestamp;
+                    isElapsed();
+                }
+            }
+        };
+
+        var render = function render(timestamp) {
+            requestAnimationFrame(render);
+            var everySecond = onElapsed(500, timestamp);
+            everySecond(function() {
+                player.move();
+            });
 
             // render the container
             renderer.render(stage);
-        }
+        };
 
-        window.setInterval(function () {
-            player.move();
+        var game = {
+            getBounds: function getBounds() {
+                return {x: gameWidth, y: gameHeight};
+            },
+            init: function init() {
+                player = window.createPlayer(loader.resources.player.texture);
 
-        }, 1000);
-        window.addEventListener('keydown', function (event) {
+                stage.addChild(player.getSprite());
 
-            player.changeDirectionByKeyCode(event.keyCode);
-        });
+                renderer.render(stage);
 
+                render();
 
-    }
+                window.addEventListener('keydown', function (event) {
+
+                    player.changeDirectionByKeyCode(event.keyCode);
+                });
+            }
+        };
+        return Object.create(game);
+    };
+
+    window.game = window.game || createGame();
+
+    loader.once('complete', game.init);
+    loader.load();
 });
