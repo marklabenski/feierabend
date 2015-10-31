a1780fb61bfa8f08aee424795ea7d26f25da17e6
marklabenski <mark.labenski@gmail.com>
2015-10-31 20:52:55 +0100

    fix: made require state aware
    Oops... I forgot to create an instance for saving
    the dependencies already loaded.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,25 +2,43 @@
 /**
  * Created by marklabenski on 31.10.15.
  */
- // pixi exposes a premade instance for you to use.
+    // pixi exposes a premade instance for you to use.
 
-window.require = function require(scripts, done) {
+var createRequire = function createRequire() {
+    window.exports = window.exports || {};
+    var requireInstance;
     var dependencies = [];
     var dfd = $.Deferred();
-    if(!Array.isArray(scripts))
-        scripts = [scripts];
-    scripts.map(function (script) {
-        if(dependencies.indexOf(script) === -1) {
-            dfd.done($.getScript("scripts/" + script));
-            dependencies.push(script);
-        }
-    });
 
-    dfd.done(function () {done()});
-    dfd.resolve();
+    var require = {
+        require: function(scripts, done) {
+            if (!Array.isArray(scripts))
+                scripts = [scripts];
+            scripts.map(function (script) {
+                if (dependencies.indexOf(script) === -1) {
+                    dfd.done($.getScript("scripts/" + script).then(function() {}));
+                    dependencies.push(script);
+                }
+            });
+
+            dfd.done(function () {
+                done()
+            });
+            dfd.resolve();
+        }
+    };
+
+    requireInstance = requireInstance || Object.create(require);
+
+    return requireInstance;
 };
 
-require('feierabend/game.js', function(){ debugger;});
+
+window.require = createRequire().require;
+
+
+require('feierabend/game.js', function () {
+});
 
 
 
