e7cfd69d75960347c061b8c0901699a5ce1879c7
MusoAga <m-oezdogan@live.de>
2015-11-22 12:01:16 +0100

    Create Menu with Start Button. Added Howlerjs for sound controll.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,10 +4,42 @@
  */
     // pixi exposes a premade instance for you to use.
 
-requirejs([ "vendor/pixijs/pixi.min", "scripts/feierabend/game.js","vendor/jquery/jquery-2.1.4.min.js"], function(pixi, game) {
+requirejs([ "vendor/pixijs/pixi.min", "scripts/feierabend/game.js", "vendor/howlerjs/howler.min.js","vendor/jquery/jquery-2.1.4.min.js"], function(pixi, game, howler) {
+
     window.feierabend = game;
 
-    $('button').on('click', function() {
-        feierabend.init();
+    // get jQuery References
+    var $menuButtons = $('.menu .buttons li');
+    var $menu = $('.menu');
+    var $canvas = $('canvas');
+
+    // If a Button Click
+    $menuButtons.on('click', function() {
+        //Check which button is clicked...
+
+        // Start Game Button
+        if($(this).attr("id") == "startButton") {
+            // Close the menu with a slideUp
+            $menu.slideUp(600, function() {
+                // Then open the Game
+                $canvas.slideDown(600, function() {
+                });
+            });
+            // Init the game...
+            feierabend.init();
+        } else if($(this).attr("id") == "highscoreButton") {
+            // do something....
+        }
+
     });
+
+    // This function is not complete
+    // This function should play a click sound if the user enters a button with the mouse
+    $menuButtons.mouseenter(function() {
+        var sound = new Howl({
+            urls: ['../sound/click.wav']
+        });
+        sound.play();
+    });
+
 });
