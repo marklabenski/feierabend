4559921d7ae46d763ca5ea7d8fa147f1d3d1b3f1
Sahagin <alexander.borunski@gmail.com>
2015-11-29 00:35:03 +0100

    edit Images

--- app/scripts/highscore.js
+++ app/scripts/highscore.js
@@ -3,41 +3,36 @@ showHighscoreList();
 function showHighscoreList () {
 
     //Test
-    localStorage.setItem("1", "Mitarbeiter 5");
-    localStorage.setItem("2", "Mitarbeiter 3");
-    localStorage.setItem("3", "Mitarbeiter 3");
-    localStorage.setItem("4", "Mitarbeiter 8");
-    localStorage.setItem("5", "Mitarbeiter 2");
-    localStorage.setItem("6", "Mitarbeiter 5");
-    localStorage.setItem("9", "Mitarbeiter 3");
-    localStorage.setItem("7", "Mitarbeiter 1");
-    localStorage.setItem("8", "Mitarbeiter 5");
-    localStorage.setItem("10", "Mitarbeiter 5");
-    localStorage.setItem("11", "Mitarbeiter 1");
+    localStorage["1"] = JSON.stringify(["8Zeichen", 27364]);
+    localStorage["2"] = JSON.stringify(["mindest.", 22339]);
+    localStorage["3"] = JSON.stringify(["sonst", 22339]);
+    localStorage["4"] = JSON.stringify(["geht die", 22339]);
+    localStorage["5"] = JSON.stringify(["Schrift", 22339]);
+    localStorage["6"] = JSON.stringify(["über das", 22339]);
+    localStorage["7"] = JSON.stringify(["das", 22339]);
+    localStorage["8"] = JSON.stringify(["Schild", 22339]);
+    localStorage["9"] = JSON.stringify(["hinaus", 22339]);
+    localStorage["10"] = JSON.stringify(["WAS GEHT", 22339]);
     //
 
-    if(typeof(Storage) !== "undefined") {
-
-        //Show #1
-        if (localStorage.getItem("1") != null) {
-            console.log(localStorage.getItem("1"));
-            $("#mitarbeiter_des_monats-name").text(localStorage.getItem("1"));
-        }
-        else
-            $("#mitarbeiter_des_monats-name").text("Niemand...");
-
+    //Show #1
+    if (localStorage.getItem("1") != null) {
+        console.log(localStorage.getItem("1"));
+        $("#mitarbeiter_des_monats-name").text(JSON.parse(localStorage[1])[0]);
+        $("#mitarbeiter_des_monats-punkte").text(JSON.parse(localStorage[1])[1]);
     }
-    else {
-        console.log("Sorry! No Web Storage support..");
-    }
+    else
+        $("#mitarbeiter_des_monats-name").text("Niemand...");
 
     //Show #2 - #10
     for (var i = 2; i <= localStorage.length && i < 11 ; i++) {
-        $("#der_rest-name ul").append("<li>"+"#"+i+" "+localStorage.getItem(i)+"</li>");
+        console.log(localStorage.getItem(i));
+        $("#der_rest-name table").append("<tr>"+"<td>#"+i+"</td><td>"+JSON.parse(localStorage[i])[0]+"</td><td>"+JSON.parse(localStorage[i])[1]+"</td></tr>");
     }
 
     //Test-end
-    localStorage.clear();
     //
 
 }
+
+
