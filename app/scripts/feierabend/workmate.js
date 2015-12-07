fe38a74f07340b9feff65c8b0c6a04c5950ffb2f
marklabenski <mark.labenski@gmail.com>
2015-12-07 10:13:37 +0100

    refactor workmate

--- app/scripts/feierabend/workmate.js
+++ app/scripts/feierabend/workmate.js
@@ -2,7 +2,7 @@ define(['scripts/feierabend/viewable.js', 'scripts/feierabend/collectable.js', '
     return function createWorkmate(number, texture, game, enterGrid) {
         var followingPlayer = null;
         var positionInQueue = 0;
-        var followPlayer= function followPlayer(player) {
+        var followPlayer = function followPlayer(player) {
             positionInQueue = player.workmatesFollowing.length;
             player.workmatesFollowing.push('workmate' + number);
             followingPlayer = player;
@@ -10,19 +10,20 @@ define(['scripts/feierabend/viewable.js', 'scripts/feierabend/collectable.js', '
         var collideFn = function collideFn(collideObj, thisWorkmate) {
             if (collideObj.id === 'player') {
                 if(followingPlayer) {
-                    console.log('player hits ' + thisWorkmate.id);
+                    //console.log('player hits ' + thisWorkmate.id);
                 } else {
                     followPlayer(collideObj);
                 }
             }
         };
         var Workmate = {
+            isSolid: true,
             move: function move() {
                 if(followingPlayer) {
-                    var followMovement1 = followingPlayer.getPathMovement(positionInQueue);
-                    var followMovement2 = followingPlayer.getPathMovement(positionInQueue+1);
-                    this.setRotationByDirection(followMovement1.direction);
-                    this.moveSpriteTo(this.getSprite(), followMovement2.tile);
+                    var queuePosDirection = followingPlayer.getPathMovement(positionInQueue);
+                    var queuePosMovement = followingPlayer.getPathMovement(positionInQueue+1);
+                    this.setRotationByDirection(queuePosDirection.direction);
+                    this.moveSpriteTo(this.getSprite(), queuePosMovement.tile);
                 }
             },
         };
