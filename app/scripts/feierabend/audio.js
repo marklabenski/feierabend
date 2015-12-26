de2ec97f93b37a686c02c108e6ff6c6cd2f6a6c5
chuxiaojiang <jcxsusan@gmail.com>
2015-12-26 14:18:28 +0100

    edit audio.js & add sound effects(officeAtomsphere, workOnPaper)

--- app/scripts/feierabend/audio.js
+++ app/scripts/feierabend/audio.js
@@ -15,11 +15,51 @@ define(["vendor/howler/howler.min.js"], function (howler) {
     volume:1.5,
     loop: false,
   });
+
+
+  var workOnNotebook = new howler.Howl({
+    urls:['audio/sigh&typing.mp3'],
+    volume:1.5,
+    loop:false,
+
+  });
+
+  var workOnPaper = new howler.Howl({
+    urls:['audio/workOnPaper2.mp3'],
+    volume:1.5,
+    loop:false,
+  });
+
+  var boss = new howler.Howl({
+    urls:['audio/boss1.mp3'],
+    volume:1.5,
+    loop:false,
+  });
+
+  var stoss = new howler.Howl({
+    urls:['audio/stoss&schrei.mp3'],
+    volume:1.5,
+    loop:false,
+  });
+
+  var officeAtmosphere = new howler.Howl({
+    urls:['audio/officeAtmosphere.mp3'],
+    volume:1,
+    loop:true,
+
+  });
+
+
+
+
+  
     
     
 
   return function playAudio(audioString) {
-    var audios = { footstep: footstep, drinkCoffee: drinkCoffee };
+    var audios = { footstep: footstep, drinkCoffee: drinkCoffee, boss: boss, stoss:stoss, 
+      workOnNotebook:workOnNotebook, workOnPaper:workOnPaper};
+
     if(audios.hasOwnProperty(audioString)) {
 
       audioToPlay = audios[audioString];
