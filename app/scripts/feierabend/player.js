5071667135495775932fa861f71a899a44ff14f2
marklabenski <mark.labenski@gmail.com>
2016-01-03 16:40:01 +0100

    add functions to unfollow player

--- app/scripts/feierabend/player.js
+++ app/scripts/feierabend/player.js
@@ -19,7 +19,17 @@ define(['scripts/feierabend/movable.js',
             },
             changeDirectionByKeyCode: function(keyCode) {
                 this.changeSpriteDirectionByKeyCode(keyCode, this.getSprite());
-            }
+            },
+            unfollowPlayer: function(workmate) {
+                var index = this.workmatesFollowing.indexOf(workmate);
+                this.workmatesFollowing.splice(index,1);
+                this.workmatesFollowing.map(function(workmate) {
+                    workmate.changeQueuePos(-1);
+                });
+            },
+            follow: function(workmate) {
+                this.workmatesFollowing.push(workmate);
+            },
         };
         var viewable = createViewable('player', texture, game.getGrid(), {x: pos.x, y:pos.y});
         viewable.init();
